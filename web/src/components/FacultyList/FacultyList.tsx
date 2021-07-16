dacb06128db662007565b4be5ed0889a8eb6af8c
Hrvoje Cosic <hcacc100@gmail.com>
2021-07-16 20:12:47 +0200

    fetch and render faculty list

--- web/src/components/FacultyList/FacultyList.tsx
+++ web/src/components/FacultyList/FacultyList.tsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './FacultyList.scss';
 
+interface Faculty {
+	ime: String;
+	grad: String;
+	id: number;
+}
+
 export const FacultyList: React.FC = () => {
+	const [faculties, setFaculties] = useState([]);
+	useEffect(() => {
+		fetch('faculties.json')
+			.then(res => {
+				return res.json();
+			})
+			.then(json => {
+				setFaculties(json);
+			});
+	}, []);
+
+	const facultyList = faculties.map((faculty: Faculty) => {
+		return <p key={faculty.id}> {faculty.ime} </p>;
+	});
+
 	return (
 		<div className='faculty-list-container'>
 			<h1>Fakulteti</h1>
-			{/* TODO: foreach fakultet... */}
-			<p>Fakultet 1</p>
-			<p>Fakultet 2</p>
-			<p>Fakultet 3</p>
-			<p>Fakultet 4</p>
-			<p>Fakultet 5</p>
+			{facultyList}
 		</div>
 	);
 };
