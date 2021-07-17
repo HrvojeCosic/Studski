3b8902dfe961a5d7c873df1b6988dd1aef22355c
Hrvoje Cosic <hcacc100@gmail.com>
2021-07-17 21:04:41 +0200

    add list of faculties seperated by areas(towns)

--- web/src/components/FacultyList/FacultyList.tsx
+++ web/src/components/FacultyList/FacultyList.tsx
@@ -2,9 +2,13 @@ import React, { useEffect, useState } from 'react';
 import './FacultyList.scss';
 
 interface Faculty {
-	ime: String;
-	grad: String;
+	ime: string;
+	grad: string;
 	id: number;
+}
+interface OrganizedFacultyList {
+	facultyArea_org: string;
+	facultiesInArea_org: string[];
 }
 
 export const FacultyList: React.FC = () => {
@@ -19,14 +23,50 @@ export const FacultyList: React.FC = () => {
 			});
 	}, []);
 
-	const facultyList = faculties.map((faculty: Faculty) => {
-		return <p key={faculty.id}> {faculty.ime} </p>;
+	//GET UNIQUE AREAS(GRADOVI) IN SEPERATE ARRAY
+	const facultyAreas: string[] = [];
+	faculties.forEach((faculty: Faculty) => {
+		if (!facultyAreas.includes(faculty.grad)) {
+			facultyAreas.push(faculty.grad);
+		}
 	});
+
+	//FOR EACH AREA(GRAD), MAKE AN OBJECT & STORE IT IN organizedFacultyList[]
+	const organizedFacultyList: OrganizedFacultyList[] = [];
+	facultyAreas.forEach(facultyArea => {
+		const facultiesInArea: string[] = [];
+		faculties.forEach((faculty: Faculty) => {
+			if (faculty.grad === facultyArea) {
+				facultiesInArea.push(faculty.ime);
+			}
+		});
+		organizedFacultyList.push({
+			facultyArea_org: facultyArea,
+			facultiesInArea_org: facultiesInArea,
+		});
+	});
+
+	//DISPLAY AREA AND LIST OF FACULTIES IN AREA, IN A SEPERATE <div>
+	const facultyListJSX = organizedFacultyList.map(
+		(obj: OrganizedFacultyList, index: number) => {
+			const facultiesForArea = obj.facultiesInArea_org.map(
+				(faculty: string, FacIndex: number) => {
+					return <p key={FacIndex}>{faculty}</p>;
+				}
+			);
+			return (
+				<div key={index}>
+					<h3>{obj.facultyArea_org}</h3>
+					{facultiesForArea}
+				</div>
+			);
+		}
+	);
 
 	return (
 		<div className='faculty-list-container'>
 			<h1>Fakulteti</h1>
-			{facultyList}
+			{facultyListJSX}
 		</div>
 	);
 };
