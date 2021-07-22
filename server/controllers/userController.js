694afd9da860a16944fbdc1193f9dae85561e3fe
Hrvoje Cosic <hcacc100@gmail.com>
2021-07-22 14:07:40 +0200

    add registration validation

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,18 +1,62 @@
 const User = require('../models/User');
-const db = require('../db');
+const bcrypt = require('bcryptjs');
 
-module.exports.createNewUser = (req, res) => {
-	const newUser = User.create({
-		username: req.body.username,
-		password: req.body.password,
-		email: req.body.email,
+module.exports.createNewUser = async (req, res) => {
+	const { username, password, repeatPassword, email } = req.body;
+	if (password != repeatPassword) {
+		res
+			.status(403)
+			.json({ title: 'error', error: 'Zaporke se ne podudaraju.' });
+		return;
+	}
+	if (password.length < 5) {
+		res
+			.status(403)
+			.json({ title: 'error', error: 'Zaporka mora imati najmanje 5 slova.' });
+		return;
+	}
+	//CHECK FOR EXISTING MAIL
+	const emailUniqueCheck = User.findAll({ where: { email: email } });
+	let emailAlreadyExists;
+	await emailUniqueCheck.then(result => {
+		result.length != 0
+			? (emailAlreadyExists = true)
+			: (emailAlreadyExists = false);
+	});
+	//CHECK FOR EXISTING USERNAME
+	const usernameUniqueCheck = User.findAll({ where: { username: username } });
+	let usernameAlreadyExists;
+	await usernameUniqueCheck.then(result => {
+		result.length != 0
+			? (usernameAlreadyExists = true)
+			: (usernameAlreadyExists = false);
+	});
+
+	if (emailAlreadyExists) {
+		res.status(403).json({ title: 'error', error: 'Taj je e-mail zauzet.' });
+		return;
+	}
+	if (usernameAlreadyExists) {
+		res
+			.status(403)
+			.json({ title: 'error', error: 'To je korisničko ime zauzeto.' });
+		return;
+	}
+	//IF EVERYTHING PASSES, CREATE USER
+	User.create({
+		username: username,
+		password: bcrypt.hashSync(password, 10),
+		email: email,
 	})
 		.then(newUser => {
-			console.log(newUser instanceof User); //todo: handle this
 			res.status(200).json({ newUser: newUser });
 		})
-		.catch(err => {
-			throw err;
+		.catch(() => {
+			res.status(400).json({
+				title: 'error',
+				error: 'Validacija neuspješna. Ispravite sva polja i pokušajte ponovo.',
+			});
+			return;
 		});
 };
 
