7676934397986701909087d408b563fe0aa3c875
Hrvoje Cosic <hcacc100@gmail.com>
2021-08-13 12:25:33 +0200

    add faculty list dropdown contents for newPostForm

--- web/src/components/CreatePostForm/CreatePostForm.tsx
+++ web/src/components/CreatePostForm/CreatePostForm.tsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import { Faculty } from '../FacultyList/FacultyList';
 import './CreatePostForm.scss';
 
 export const CreatePostForm: React.FC<{ faculties: Array<Faculty> }> = ({
 	faculties,
 }) => {
+	const facultyAreasSet = new Set(
+		faculties.map(faculty => {
+			return faculty.grad;
+		})
+	);
+	const facultyAreas = Array.from(facultyAreasSet).map(area => {
+		return <option key={area}>{area}</option>;
+	});
+
+	let facultyNameList: Array<JSX.Element> = [];
+	const updateFacultyList = (event: ChangeEvent<HTMLSelectElement>) => {
+		const selectedArea = event.target.value;
+		facultyNameList = faculties.map(faculty => {
+			return <option>{faculty.ime}</option>;
+		});
+	};
+
 	return (
 		<div className='post-form-container'>
+			<h3>Grad</h3>
+			<select onChange={updateFacultyList}>{facultyAreas}</select>
 			<h3>Fakultet</h3>
 			<select>
 				{/* TODO: make user's faculty the first option */}
-				<option>fakultet 1...</option>
+				<option>{facultyNameList}</option>
 			</select>
 			<input type='text' placeholder='Naslov' />
 			<input type='file' name='datoteka' />
