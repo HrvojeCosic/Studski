60d0225d9473ae64486722fec5e59bb06a398501
HrvojeCosic <hcacc100@gmail.com>
2021-08-16 11:35:12 +0200

    fix: display only selected area's faculties in CreatePostForm

--- web/src/components/CreatePostForm/CreatePostForm.tsx
+++ web/src/components/CreatePostForm/CreatePostForm.tsx
@@ -1,10 +1,12 @@
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { Faculty } from '../FacultyList/FacultyList';
 import './CreatePostForm.scss';
 
 export const CreatePostForm: React.FC<{ faculties: Array<Faculty> }> = ({
 	faculties,
 }) => {
+	const [formFacultyList, setFormFacultyList] = useState([<option key="defaultKey"></option>])
+
 	const facultyAreasSet = new Set(
 		faculties.map(faculty => {
 			return faculty.grad;
@@ -14,22 +16,30 @@ export const CreatePostForm: React.FC<{ faculties: Array<Faculty> }> = ({
 		return <option key={area}>{area}</option>;
 	});
 
-	let facultyNameList: Array<JSX.Element> = [];
 	const updateFacultyList = (event: ChangeEvent<HTMLSelectElement>) => {
 		const selectedArea = event.target.value;
-		facultyNameList = faculties.map(faculty => {
-			return <option>{faculty.ime}</option>;
+		const list: Array<JSX.Element> = faculties.map(faculty => {
+			if (faculty.grad === selectedArea) {
+				return <option selected key={selectedArea+faculty.id}>{faculty.ime}</option>;
+			} 
+			//return option element just to make typescript happy
+			else return <option key={selectedArea+faculty.id} style={{display: "none"}}></option>
 		});
+		setFormFacultyList(list)
 	};
 
 	return (
 		<div className='post-form-container'>
 			<h3>Grad</h3>
-			<select onChange={updateFacultyList}>{facultyAreas}</select>
+			<select onChange={updateFacultyList}>
+			<option selected disabled hidden></option> 
+			{facultyAreas}
+			</select>
+			
 			<h3>Fakultet</h3>
 			<select>
 				{/* TODO: make user's faculty the first option */}
-				<option>{facultyNameList}</option>
+				{formFacultyList}
 			</select>
 			<input type='text' placeholder='Naslov' />
 			<input type='file' name='datoteka' />
