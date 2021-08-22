4f04812af16f0f6be3399140f0d1b42b60107454
HrvojeCosic <hcacc100@gmail.com>
2021-08-22 15:23:54 +0200

    fix: change database collate option to support Croatian and a wider range of characters

--- server/db.js
+++ server/db.js
@@ -6,6 +6,10 @@ module.exports = new Sequelize(
 	process.env.USER_DB,
 	process.env.PASSWORD_DB,
 	{
+		define: {
+			charset: 'utf8',
+			collate: 'utf8_unicode_ci',
+		},
 		host: process.env.HOST_DB,
 		dialect: 'mysql',
 		//createdAt and updatedAt automatic columns are ENABLED!
